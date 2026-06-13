60ee78e502ba2011342e6b344695c198df893125
iamRaaz-3 <reeshiraj003@gmail.com>
2026-06-13 09:05:17 +0530

    feat: mount Features component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from './components/Hero/Hero.jsx'
 import Ticker from './components/Ticker/Ticker.jsx'
+import Features from './components/Features/Features.jsx'
 
 export default function App() {
   return (
     <>
       <Hero />
       <Ticker />
+      <Features />
     </>
   )
 }
