ed87b27bb4d61ad66ead216444db5604ceb8a02f
iamRaaz-3 <reeshiraj003@gmail.com>
2026-06-13 09:32:53 +0530

    wire Partners section and fix asset import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Hero from './components/Hero/Hero.jsx'
 import Ticker from './components/Ticker/Ticker.jsx'
 import Features from './components/Features/Features.jsx'
+import Partners from './components/Partners/Partners.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
 
 export default function App() {
   return (
     <>
+      <Navbar />
       <Hero />
       <Ticker />
+      <Partners />
       <Features />
     </>
-  )
+  );
 }
